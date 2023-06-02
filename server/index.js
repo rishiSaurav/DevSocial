52b5543c1d9730d5d62a8e60f106f5d1d274113b
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-06-02 09:19:49 +0530

    Added New Data for storage of files

--- server/index.js
+++ server/index.js
@@ -20,5 +20,15 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors())
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')))
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename : function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+const upload = multer ({ storage })
